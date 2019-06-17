b491065ec575447af6f489ab5232d931d7cb446a
Dmitry Svirbutovich <bixboxdima@gmail.com>
2019-06-17 16:28:55 +0300

    Добавил CopyWebpackPlugin, оформление и картинки

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -43,5 +44,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html',
     }),
+    new CopyWebpackPlugin([{ from: 'src/images', to: 'images' }]),
   ],
 };
