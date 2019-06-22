fb175025d5e8e7ec985127e79fb53df37db9a131
Dmitry Svirbutovich <bixboxdima@gmail.com>
2019-06-22 23:05:45 +0300

    Изменил хранение данных

--- src/index.js
+++ src/index.js
@@ -4,43 +4,157 @@ import Model from './js/model';
 import View from './js/view';
 import Controller from './js/controller';
 
-const allItem = [
-  { name: 'Подорожник', img: '/images/icon/001-plant-2.png' },
-  { name: 'Пижма', img: '/images/icon/002-oxygen.png' },
-  { name: 'Копытень', img: '/images/icon/003-leaf.png' },
-  { name: 'Базилик', img: '/images/icon/004-basil.png' },
-  { name: 'Арктоус', img: '/images/icon/005-food-and-restaurant.png' },
-  { name: 'Чистотелел', img: '/images/icon/006-leaf-1.png' },
-  { name: 'Цикорий', img: '/images/icon/007-plant.png' },
-  { name: 'Полынь', img: '/images/icon/008-leaf-2.png' },
-  { name: 'Окопник', img: '/images/icon/009-plant-1.png' },
-  { name: 'Зверобой', img: '/images/icon/010-spice.png' },
-  { name: 'Лебеда', img: '/images/icon/011-leaf-3.png' },
-  { name: 'Душица', img: '/images/icon/012-food-and-restaurant-1.png' },
-  { name: 'Адский гриб', img: '/images/icon/013-mushroom.png' },
-  { name: 'Боровик', img: '/images/icon/014-mushroom-1.png' },
-  { name: 'Мухомор', img: '/images/icon/015-mushroom-2.png' },
-  { name: 'Лунная тень', img: '/images/icon/016-green-tea.png' },
-  { name: 'Целебная трава', img: '/images/icon/017-peppermint.png' },
-  { name: 'Зелье Счастья', img: '/images/icon/018-poison.png' },
-  { name: 'Зелье Здоровья', img: '/images/icon/019-potion.png' },
-  { name: 'Яд', img: '/images/icon/020-potion-1.png' },
-  { name: 'Зелье Смеха', img: '/images/icon/021-potion-2.png' },
-  { name: 'Зелье Взрыва', img: '/images/icon/022-antidote.png' },
-  { name: 'Зелье Жизни', img: '/images/icon/023-potion-3.png' },
-  { name: 'Зелье Удачи', img: '/images/icon/024-potion-4.png' },
+const items = [
+  {
+    name: 'Подорожник',
+    img: '/images/icon/001-plant-2.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Пижма',
+    img: '/images/icon/002-oxygen.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Копытень',
+    img: '/images/icon/003-leaf.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Базилик',
+    img: '/images/icon/004-basil.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Арктоус',
+    img: '/images/icon/005-food-and-restaurant.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Чистотелел',
+    img: '/images/icon/006-leaf-1.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Цикорий',
+    img: '/images/icon/007-plant.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Полынь',
+    img: '/images/icon/008-leaf-2.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Окопник',
+    img: '/images/icon/009-plant-1.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Зверобой',
+    img: '/images/icon/010-spice.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Лебеда',
+    img: '/images/icon/011-leaf-3.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Душица',
+    img: '/images/icon/012-food-and-restaurant-1.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Адский гриб',
+    img: '/images/icon/013-mushroom.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Боровик',
+    img: '/images/icon/014-mushroom-1.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Мухомор',
+    img: '/images/icon/015-mushroom-2.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Лунная тень',
+    img: '/images/icon/016-green-tea.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Целебная трава',
+    img: '/images/icon/017-peppermint.png',
+    recipe: null,
+    isOpen: true,
+  },
+  {
+    name: 'Зелье Счастья',
+    img: '/images/icon/018-poison.png',
+    recipe: ['Арктоус', 'Пижма'],
+    isOpen: false,
+  },
+  {
+    name: 'Зелье Здоровья',
+    img: '/images/icon/019-potion.png',
+    recipe: ['Подорожник', 'Базилик', 'Чистотелел'],
+    isOpen: false,
+  },
+  {
+    name: 'Яд',
+    img: '/images/icon/020-potion-1.png',
+    recipe: ['Мухомор', 'Мухомор', 'Боровик'],
+    isOpen: false,
+  },
+
+  {
+    name: 'Зелье Смеха',
+    img: '/images/icon/021-potion-2.png',
+    recipe: ['Окопник', 'Базилик', 'Пижма'],
+    isOpen: false,
+  },
+
+  {
+    name: 'Зелье Взрыва',
+    img: '/images/icon/022-antidote.png',
+    recipe: ['Мухомор', 'Лебеда', 'Арктоус'],
+    isOpen: false,
+  },
+
+  {
+    name: 'Зелье Жизни',
+    img: '/images/icon/023-potion-3.png',
+    recipe: ['Целебная трава', 'Целебная трава', 'Адский гриб'],
+    isOpen: false,
+  },
+  {
+    name: 'Зелье Удачи',
+    img: '/images/icon/024-potion-4.png',
+    recipe: ['Цикорий', 'Лунная тень', 'Зелье Счастья'],
+    isOpen: false,
+  },
 ];
-const startIdItem = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-const recipes = [
-  { newItem: 17, needItemCraft: [4, 1] },
-  { newItem: 18, needItemCraft: [0, 3, 5] },
-  { newItem: 19, needItemCraft: [14, 14, 13] },
-  { newItem: 20, needItemCraft: [8, 3, 1] },
-  { newItem: 21, needItemCraft: [14, 10, 4] },
-  { newItem: 22, needItemCraft: [16, 16, 12] },
-  { newItem: 23, needItemCraft: [6, 15, 17] },
-];
-const model = new Model(allItem, startIdItem, recipes);
+
+const model = new Model(items);
 const view = new View();
 // eslint-disable-next-line no-unused-vars
 const controller = new Controller(model, view);
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,54 +1,65 @@
+// События
+const CRAFT = 'craft';
+const HIGHLIGHT_ITEMS_RECIPE = 'highlightItemsRecipe';
+const CREATE_MODEL = 'createModel';
+const CREATE_RECIPE = 'createRecipe';
+
 class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
 
-    view.initializeElements(model.item, model.allItem, model.recipes);
-
-    view.on('craft', this.newCraft.bind(this));
-    view.on('createModel', this.modalCreateContent.bind(this));
-    view.on('createRecept', this.createRecept.bind(this));
-    view.on('getIdItemRecept', this.getIdItemRecept.bind(this));
+    this.view.initElements(model.items);
+    this.initListener();
   }
 
-  newCraft({ recipesId, idItemOnTable }) {
-    const result = this.model.craftNewItem(recipesId, idItemOnTable);
+  initListener() {
+    this.view.on(CRAFT, this.newCraft);
+    this.view.on(CREATE_MODEL, this.modalCreateContent);
+    this.view.on(CREATE_RECIPE, this.createRecept);
+    this.view.on(HIGHLIGHT_ITEMS_RECIPE, this.highlightItemsRecipe);
+  }
+
+  newCraft = ({ itemCreateName, nameItemOnTable }) => {
+    const result = this.model.craftNewItem(itemCreateName, nameItemOnTable);
     if (result.messageType) {
       return this.view.showMessage(result);
     }
 
-    return this.view.successCreate(recipesId, result);
-  }
+    return this.view.successCreate(itemCreateName, result);
+  };
 
-  createRecept(newRecept) {
-    const result = this.model.newRecept(newRecept);
+  createRecept = newRecept => {
+    const result = this.model.addNewRecept(newRecept);
     if (result.messageType) {
       return this.view.showMessage(result);
     }
 
-    const { id, name, img } = result;
+    const { name, img } = result;
 
-    this.view.createNewRecipes(id, name, img);
+    this.view.createNewRecipes(name, img);
     this.view.modalHide();
     return this.view.showMessage({ messageType: 'success', message: 'Новый рецепт создан' });
-  }
+  };
 
-  modalCreateContent() {
-    this.view.modalCreateNewRecept(this.model.allItem);
-  }
+  modalCreateContent = () => {
+    this.view.modalCreateNewRecept(this.model.items);
+  };
 
-  getIdItemRecept(idRecept) {
-    const listItem = this.model.recipes[idRecept];
+  highlightItemsRecipe = nameItem => {
+    const { recipe } = this.model.items.get(nameItem);
 
-    const itemInfo = listItem.needItemCraft.reduce((arr, id) => {
-      if (!this.model.item.includes(id)) {
-        arr.push(this.model.getInfoItem(id));
+    const itemInfo = recipe.reduce((acc, name) => {
+      if (!this.model.items.get(name).isOpen) {
+        const img = this.model.getImgItem(name);
+
+        acc.push({ name, img });
       }
-      return arr;
+      return acc;
     }, []);
 
-    this.view.highlightItem(listItem, itemInfo);
-  }
+    this.view.highlightItem(recipe, itemInfo);
+  };
 }
 
 export default Controller;
